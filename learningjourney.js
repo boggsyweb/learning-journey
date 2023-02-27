2009dd6edf65155e30f0c8d0d8db91ebe0d86f10
bosspigg <123977427+boggsyweb@users.noreply.github.com>
2023-02-27 17:30:58 +0000

    initial commits

---
+++ learningjourney.js
@@ -0,0 +1,38 @@
+anime.timeline({})
+  .add({
+    targets: '.animation .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 700
+  }).add({
+    targets: '.animation .line',
+    duration: 600,
+    easing: "easeOutExpo",
+    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+  }).add({
+    targets: '.animation .icon',
+    opacity: [0,1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  }).add({
+    targets: '.animation .letters-left',
+    opacity: [0,1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=300'
+  }).add({
+    targets: '.animation .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  })
+  
+let date = (new Date()).getFullYear()
+document.getElementById('year').innerHTML = date
+  
